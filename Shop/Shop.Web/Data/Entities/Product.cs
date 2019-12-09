23dbbc9cefbcc1366318696c8493acbb10988a9b
Diego Pardo <dlplenin@yahoo.es>
2019-12-09 16:39:10 -0500

    Repository Pattern

--- Shop/Shop.Web/Data/Entities/Product.cs
+++ Shop/Shop.Web/Data/Entities/Product.cs
@@ -11,7 +11,6 @@ namespace Shop.Web.Data.Entities
         public int Id { get; set; }
         [MaxLength(50, ErrorMessage = "The field {0} only can contain a maximum {1} characters")]
         [Required]
-
         public string Name { get; set; }
         [DisplayFormat(DataFormatString = "{0:C2}", ApplyFormatInEditMode = false)]
         public decimal Price { get; set; }
---
+++ Shop/Shop.Web/Data/MockRepository.cs
@@ -0,0 +1,51 @@
+﻿using Shop.Web.Data.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Shop.Web.Data
+{
+    //public class MockRepository : IRepository
+    //{
+    //    public void AddProduct(Product product)
+    //    {
+    //        throw new NotImplementedException();
+    //    }
+
+    //    public Product GetProduct(int id)
+    //    {
+    //        throw new NotImplementedException();
+    //    }
+
+    //    public IEnumerable<Product> GetProducts()
+    //    {
+    //        return new List<Product>()
+    //        {
+    //            new Product() { Id = 1, Name = "One", Price = 10 },
+    //            new Product() { Id = 2, Name = "Two", Price = 20 },
+    //            new Product() { Id = 3, Name = "Three", Price = 30 }
+    //        };
+    //    }
+
+    //    public bool ProductExists(int id)
+    //    {
+    //        throw new NotImplementedException();
+    //    }
+
+    //    public void RemoveProduct(Product product)
+    //    {
+    //        throw new NotImplementedException();
+    //    }
+
+    //    public Task<bool> SaveAllAsync()
+    //    {
+    //        throw new NotImplementedException();
+    //    }
+
+    //    public void UpdateProduct(Product product)
+    //    {
+    //        throw new NotImplementedException();
+    //    }
+    //}
+}
