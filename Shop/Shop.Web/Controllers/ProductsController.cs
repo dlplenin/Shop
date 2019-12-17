c8e03c2071d4e465f5745f76a13894a587b1e377
Diego Pardo <dlplenin@yahoo.es>
2019-12-17 11:54:53 -0500

    LogIn page&Authorize

--- Shop/Shop.Web/Controllers/ProductsController.cs
+++ Shop/Shop.Web/Controllers/ProductsController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
@@ -13,6 +14,7 @@ using Shop.Web.Models;
 
 namespace Shop.Web.Controllers
 {
+    [Authorize]
     public class ProductsController : Controller
     {
         private readonly IProductRepository productRepository;
