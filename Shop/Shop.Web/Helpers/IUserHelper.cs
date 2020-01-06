2b4c1d4cc3de947969f53fb64164053d88255238
Diego Pardo <dlplenin@yahoo.es>
2020-01-06 17:19:40 -0500

    Recover password

--- Shop/Shop.Web/Helpers/IUserHelper.cs
+++ Shop/Shop.Web/Helpers/IUserHelper.cs
@@ -30,5 +30,8 @@ namespace Shop.Web.Helpers
         Task<string> GenerateEmailConfirmationTokenAsync(User user);
         Task<IdentityResult> ConfirmEmailAsync(User user, string token);
         Task<User> GetUserByIdAsync(string userId);
+
+        Task<string> GeneratePasswordResetTokenAsync(User user);
+        Task<IdentityResult> ResetPasswordAsync(User user, string token, string password);
     }
 }
---
+++ Shop/Shop.Web/Models/RecoverPasswordViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Shop.Web.Models
+{
+    public class RecoverPasswordViewModel
+    {
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+    }
+
+}
