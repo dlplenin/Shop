7da488a1a3ddef2bcb073212943edef5ad8323f9
Diego Pardo <dlplenin@yahoo.es>
2020-01-10 12:18:50 -0500

    CRUD API

--- Shop/Shop.Web/Controllers/API/ProductsController.cs
+++ Shop/Shop.Web/Controllers/API/ProductsController.cs
@@ -2,6 +2,8 @@
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Shop.Web.Data;
+using Shop.Web.Data.Entities;
+using Shop.Web.Helpers;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,24 +11,105 @@ using System.Threading.Tasks;
 
 namespace Shop.Web.Controllers.API
 {
-    [Route("api/[Controller]")]
-    [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
-    public class ProductsController : Controller
-    {
-        private readonly IProductRepository productRepository;
+	[Route("api/[Controller]")]
+	[Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
+	public class ProductsController : Controller
+	{
+		private readonly IProductRepository productRepository;
+		private readonly IUserHelper userHelper;
 
-        public ProductsController(IProductRepository productRepository)
-        {
-            this.productRepository = productRepository;
-        }
+		public ProductsController(IProductRepository productRepository, IUserHelper userHelper)
+		{
+			this.productRepository = productRepository;
+			this.userHelper = userHelper;
+		}
 
-        [HttpGet]
-        public IActionResult GetProducts()
-        {
-            var products = this.productRepository.GetAllWithUsers().OrderBy(x => x.Name);
+		[HttpGet]
+		public IActionResult GetProducts()
+		{
+			var products = this.productRepository.GetAllWithUsers().OrderBy(x => x.Name);
   
-            return this.Ok(products);
-        }
+			return this.Ok(products);
+		}
 
-    }
+		[HttpPost]
+		public async Task<IActionResult> PostProduct([FromBody] Common.Models.Product product)
+		{
+			if (!ModelState.IsValid)
+			{
+				return this.BadRequest(ModelState);
+			}
+
+			var user = await this.userHelper.GetUserByEmailAsync(product.User.Email);
+			if (user == null)
+			{
+				return this.BadRequest("Invalid user");
+			}
+
+			//TODO: Upload images
+			var entityProduct = new Product
+			{
+				IsAvailabe = product.IsAvailabe,
+				LastPurchase = product.LastPurchase,
+				LastSale = product.LastSale,
+				Name = product.Name,
+				Price = product.Price,
+				Stock = product.Stock,
+				User = user
+			};
+
+			var newProduct = await this.productRepository.CreateAsync(entityProduct);
+			return Ok(newProduct);
+		}
+
+		[HttpPut("{id}")]
+		public async Task<IActionResult> PutProduct([FromRoute] int id, [FromBody] Common.Models.Product product)
+		{
+			if (!ModelState.IsValid)
+			{
+				return this.BadRequest(ModelState);
+			}
+
+			if (id != product.Id)
+			{
+				return BadRequest();
+			}
+
+			var oldProduct = await this.productRepository.GetByIdAsync(id);
+			if (oldProduct == null)
+			{
+				return this.BadRequest("Product Id don't exists.");
+			}
+
+			//TODO: Upload images
+			oldProduct.IsAvailabe = product.IsAvailabe;
+			oldProduct.LastPurchase = product.LastPurchase;
+			oldProduct.LastSale = product.LastSale;
+			oldProduct.Name = product.Name;
+			oldProduct.Price = product.Price;
+			oldProduct.Stock = product.Stock;
+
+			var updatedProduct = await this.productRepository.UpdateAsync(oldProduct);
+			return Ok(updatedProduct);
+		}
+
+		[HttpDelete("{id}")]
+		public async Task<IActionResult> DeleteProduct([FromRoute] int id)
+		{
+			if (!ModelState.IsValid)
+			{
+				return this.BadRequest(ModelState);
+			}
+
+			var product = await this.productRepository.GetByIdAsync(id);
+			if (product == null)
+			{
+				return this.NotFound();
+			}
+
+			await this.productRepository.DeleteAsync(product);
+			return Ok(product);
+		}
+
+	}
 }
